e9b84ba4ef3ed36320c2dee37e3d2c10eedad2a2
NguyenUzuu <nguyen12112005@gmail.com>
2026-08-07 13:39:51 +0700

    [ADD] splash & after splash screen

---
+++ lib/features/start/first/after_splash_screen.dart
@@ -0,0 +1,71 @@
+
+import 'package:duolingo_ui_clone/core/theme/app_colors.dart';
+import 'package:duolingo_ui_clone/core/theme/app_icon.dart';
+import 'package:duolingo_ui_clone/core/theme/app_images.dart';
+import 'package:duolingo_ui_clone/core/theme/app_spacing.dart';
+import 'package:duolingo_ui_clone/core/theme/app_text_theme.dart';
+import 'package:duolingo_ui_clone/core/widgets/app_button.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class AfterSplashScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Padding(
+        padding: EdgeInsets.all(AppSpacing.lg),
+        child: Expanded(
+            child: Column(
+
+              children: [
+                Expanded(
+
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          AppImages.duo_after_splash,
+                          width: 100,
+                        ),
+                        SizedBox(height: AppSpacing.lg,),
+
+                        SvgPicture.asset(AppIcon.logoTextGreen, width: 120,),
+                        SizedBox(height: AppSpacing.sm  ,),
+
+                        Text('Học miễn phí. Suốt đời',
+                          style: AppTextTheme.light.titleSmall,
+                        ),
+                      ],
+                    )
+                ),
+
+                Column(
+                  children: [
+                    AppButton(
+                      label: 'BẮT ĐẦU NGAY',
+                      onPressed: () => {},
+                      textColor: AppColors.background,
+                    ),
+
+                    SizedBox(height: AppSpacing.lg,),
+
+                    AppButton(
+                      label: 'TÔI ĐÃ CÓ TÀI KHOẢN',
+                      onPressed: () => {},
+                      backgroundColor: AppColors.background,
+                      textColor: AppColors.duoGreen,
+                      borderColor: AppColors.grayBorder200,
+                      depthColor: AppColors.grayBorder200,
+                    ),
+                  ],
+                )
+              ],
+            )
+        ),
+      ),
+    );
+  }
+}
